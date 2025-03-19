fdba6d65d969c1074d051fb6ca74f59e1809259f
Benjamin Whalley <benjaminjwhalley@outlook.com>
2025-03-19 18:34:52 +0000

    tests@linked_list: using with generics

--- tests/linked_list_tests.cpp
+++ tests/linked_list_tests.cpp
@@ -8,21 +8,21 @@
 #include "node.h"
 
 TEST(LinkedListTest, GetNode) {
-    auto* linked_list = new LinkedList();
+    auto* linked_list = new LinkedList<int>();
     auto* newNode = new Node(4);
     linked_list->insert(newNode);
-    Node* gotten_node = linked_list->get(0);
+    Node<int>* gotten_node = linked_list->get(0);
     ASSERT_EQ(gotten_node, newNode);
 }
 
 TEST(LinkedListTest, GetNodeLargeIndex) {
-    auto* linked_list = new LinkedList();
-    linked_list->insert(new Node(4));
+    auto* linked_list = new LinkedList<int>();
+    linked_list->insert(new Node<int>(4));
     ASSERT_EQ(linked_list->get(1), nullptr);
 }
 
 TEST(LinkedListTest, GetNodeSmallIndex) {
-    auto* linked_list = new LinkedList();
-    linked_list->insert(new Node(4));
+    auto* linked_list = new LinkedList<int>();
+    linked_list->insert(new Node<int>(4));
     ASSERT_EQ(linked_list->get(-1), nullptr);
 }
