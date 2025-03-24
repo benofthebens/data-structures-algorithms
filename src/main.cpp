da8b181432c685ee48e6043f68784bf7d97a91e6
Benjamin Whalley <benjaminjwhalley@outlook.com>
2025-03-24 17:44:07 +0000

    updated main

--- src/main.cpp
+++ src/main.cpp
@@ -7,15 +7,11 @@
 #include "queue.h"
 #include "stack.h"
 #include "tree.h"
-#include "../exceptions/stack_overflow_exception.h"
-#include "../exceptions/stack_underflow_exception.h"
 
 int main() {
     Tree<int>* tree = new Tree<int>();
-    int data = 4;
-    tree->add(0, data);
-    tree->add(4, 5);
-
-
+    tree->add(0, 4);
+    tree->add(4,5);
+    std::cout << tree->get(4) << std::endl;
     return 0;
 }
