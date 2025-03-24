2705995ec06593757b5bd76c258a55a06cf08b9a
Benjamin Whalley <benjaminjwhalley@outlook.com>
2025-03-24 17:42:58 +0000

    feature@tree: added breadfirst search to search elements in the tree

--- src/tree.cpp
+++ src/tree.cpp
@@ -1,31 +1,58 @@
 #include "tree.h"
+
+#include "queue.h"
 //
 // Created by benja on 20/03/2025.
 //
 template<typename T>
 Tree<T>::Tree() {
-    this->graph = new Graph<Node<T>*>();
     this->root = nullptr;
 }
 
 template <typename T>
 Tree<T>::~Tree() {
-    delete graph;
     delete root;
 }
 
 template<typename T>
 void Tree<T>::add(T parent_data, T data) {
-    graph->add_node(new Node(data));
     if (root == nullptr) {
-        root = graph->get_node_at(0)->get_data();
+        root = new Node(data);
         return;
     }
-    Node<T>* parent = graph->get_node(new Node(parent_data))->get_data();
-    parent->add_edge(new Node(data));
+    Node<T>* parent = get(parent_data);
+    parent->add_edge(data);
 }
 
 template<typename T>
 T Tree<T>::remove() {
+
+
 }
+
+template<typename T>
+Node<T> *Tree<T>::get(T data) {
+    auto* current = root;
+    auto* queue = new Queue<Node<T>*>(200);
+    queue->enqueue(current);
+
+    while (!queue->is_empty()) {
+        current = queue->dequeue();
+        std::vector<Node<T>*>* successors = current->get_all_pointers();
+        if (current->eq(new Node(data))) {
+            delete queue;
+            return current;
+        }
+        for (int i = 0; i < successors->size(); i++) {
+            if (successors->at(i)->eq(new Node(data))) {
+                delete queue;
+                return successors->at(i);
+            }
+            queue->enqueue(successors->at(i));
+        }
+    }
+    delete queue;
+    return nullptr;
+}
+
 template class Tree<int>;
