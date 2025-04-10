92495a89a335ebe205905849a5e1f3d019ed5f6f
benjamin whalley <benjaminjwhalley@outlook.com>
2025-04-10 15:46:01 +0100

    chore@tests: Redo of tests

--- tests/node_tests.cpp
+++ tests/node_tests.cpp
@@ -2,4 +2,26 @@
 // Created by benja on 14/03/2025.
 //
 #include <gtest/gtest.h>
+#include "node.h"
 
+class NodeTest : public testing::Test {
+protected:
+    NodeTest() {
+    }
+};
+
+TEST_F(NodeTest, Assign) {
+
+}
+
+TEST_F(NodeTest, AddAdjacentNode) {
+
+}
+
+TEST_F(NodeTest, EqualOperator) {
+
+}
+
+TEST_F(NodeTest, SetNumberOfAdjacentNodes) {
+
+}
